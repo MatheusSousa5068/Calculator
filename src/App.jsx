f29b69c790ab669e2459d9fd10bf90172dad1582
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2020-07-31 11:56:21 -0300

    rendering of numbers and operators finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,37 @@ import React from 'react';
 // Imports
 import Button from './components/Button'
 import Input from './components/Input'
-import Clear from './components/Clear'
+//import Clear from './components/Clear'
 
 export default props =>
   <div className='App'>
     <div className="calc-wrapper">
-      test
+      <div className="row">
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
+        <Button>/</Button>
+      </div>
+
+      <div className="row">
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button>&times;</Button>
+      </div>
+
+      <div className="row">
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button>+</Button>
+      </div>
+
+      <div className="row">
+        <Button>.</Button>
+        <Button>0</Button>
+        <Button>=</Button>
+        <Button>-</Button>
+      </div>
     </div>
   </div>
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import './index.css'
+import "./index.css";
 
-export default props =>
-    <>
-        <div>
-            {props.children}
-        </div>
-    </>
+const isOperator = (val) => {
+  return !isNaN(val) || val === "." || val === "=";
+};
+
+export default (props) => (
+  <>
+    <div
+      className={`button-wrapper ${
+        isOperator(props.children) ? null : "operator"
+      }`}
+    >
+      {props.children}
+    </div>
+  </>
+);
